38c4cbaa8b472be8edf2c812a75edd1eb912c40c
juliomonteiiro <julioalexsandro.monteiro13@gmail.com>
2025-07-22 10:53:52 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Screen Sound
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound!";
+Console.WriteLine(mensagemDeBoasVindas);
+
+string nome = "Julio";
+string sobrenome = "Monteiro";
+
+Console.WriteLine(nome + sobrenome);
+Console.WriteLine($"{nome} {sobrenome}");
+Console.WriteLine(nome + " " + sobrenome);
+Console.WriteLine(string.Concat(nome, " ", sobrenome));
+
