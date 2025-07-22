9ce86de62e0b2078afe3573af28a86e9e30d89d4
juliomonteiiro <julioalexsandro.monteiro13@gmail.com>
2025-07-22 12:16:15 -0300

    feat: module 2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,49 @@
 ﻿// Screen Sound
-string mensagemDeBoasVindas = "Boas vindas ao Screen Sound!";
-Console.WriteLine(mensagemDeBoasVindas);
+string mensagemDeBoasVindas = "Boas vindas ao Screen Sound";
+List<string> listaDasBandas = new List<string> { "Red Hot Chili Peppers", "Linkin Park", "Guns N' Roses"};  
 
-string nome = "Julio";
-string sobrenome = "Monteiro";
+void ExibirLogo()
+{
+    Console.WriteLine(@"
 
-Console.WriteLine(nome + sobrenome);
-Console.WriteLine($"{nome} {sobrenome}");
-Console.WriteLine(nome + " " + sobrenome);
-Console.WriteLine(string.Concat(nome, " ", sobrenome));
+░██████╗░█████╗░██████╗░███████╗███████╗███╗░░██╗  ░██████╗░█████╗░██╗░░░██╗███╗░░██╗██████╗░
+██╔════╝██╔══██╗██╔══██╗██╔════╝██╔════╝████╗░██║  ██╔════╝██╔══██╗██║░░░██║████╗░██║██╔══██╗
+╚█████╗░██║░░╚═╝██████╔╝█████╗░░█████╗░░██╔██╗██║  ╚█████╗░██║░░██║██║░░░██║██╔██╗██║██║░░██║
+░╚═══██╗██║░░██╗██╔══██╗██╔══╝░░██╔══╝░░██║╚████║  ░╚═══██╗██║░░██║██║░░░██║██║╚████║██║░░██║
+██████╔╝╚█████╔╝██║░░██║███████╗███████╗██║░╚███║  ██████╔╝╚█████╔╝╚██████╔╝██║░╚███║██████╔╝
+╚═════╝░░╚════╝░╚═╝░░╚═╝╚══════╝╚══════╝╚═╝░░╚══╝  ╚═════╝░░╚════╝░░╚═════╝░╚═╝░░╚══╝╚═════╝░
+");
+    Console.WriteLine(mensagemDeBoasVindas);
+}
 
+void ExibirOpcoesDoMenu()
+{
+    ExibirLogo()
+    Console.WriteLine("\nDigite 1 para registrar uma banda");
+    Console.WriteLine("Digite 2 para mostrar todas as bandas");
+    Console.WriteLine("Digite 3 para avaliar uma banda");
+    Console.WriteLine("Digite 4 para exibir a média de uma banda");
+    Console.WriteLine("Digite -1 para sair");
+
+    Console.Write("\nDigite a sua opção: ");
+    string opcaoEscolhida = Console.ReadLine();
+    int opcaoEscolhidaNumerica = int.Parse(opcaoEscolhida);
+
+    switch (opcaoEscolhidaNumerica)
+    {
+        case 1: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 2: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 3: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case 4: Console.WriteLine("Você escolheu a opção " + opcaoEscolhidaNumerica);
+            break;
+        case -1: Console.WriteLine("Tchau Tchau :)");
+            break;
+        default: Console.WriteLine("Opção inválida");
+            break;
+    }
+}
+
+ExibirOpcoesDoMenu();
