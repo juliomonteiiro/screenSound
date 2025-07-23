6833e98feb51a42c867d288463cbb8d2ac4a6439
juliomonteiiro <julioalexsandro.monteiro13@gmail.com>
2025-07-23 12:46:45 -0300

    screendSoundV2: module 1

---
+++ screenSoundV2/Program.cs
@@ -0,0 +1,14 @@
+﻿Musica musica1 = new Musica();
+musica1.nome = "This I Love";
+musica1.artista = "Guns N' Roses";
+musica1.duracao = 535;
+musica1.disponivel = true; 
+
+Musica musica2 = new Musica();
+musica2.nome = "Californication";
+musica2.artista= "Red Hot Chilli Peppers";
+musica2.duracao = 458;
+musica2.disponivel = false; 
+
+musica1.ExibirFichaTecnica();
+musica2.ExibirFichaTecnica();
